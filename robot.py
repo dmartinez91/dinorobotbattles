f14ee124b5e5104c35729737684495c1e241b372
Daniel Martinez <danielmartinez@Daniels-MacBook-Pro.local>
2021-07-15 15:08:37 -0400

    added robot class

---
+++ robot.py
@@ -0,0 +1,16 @@
+class Robot():
+    def __init__(self, name, weapon):
+        self.robot_name = name
+        self.health = 0
+        self.robot_weapon = weapon
+
+    def __str__(self):
+        return f' {self.robot_name} {self.health} {self.robot_weapon}' 
+    
+    def attack(self, dinosaur):
+        self.attacking = dinosaur
+
+
+robot1 = Robot('mecha godzilla', Weapon("RayGun", 5))
+
+print(robot1)
